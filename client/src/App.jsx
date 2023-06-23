57427f73f7763ec701fde3565e8edcfe91333e21
Arjun <arjunqwerty4703@gmail.com>
2023-06-23 22:16:23 +0530

    login done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import RegForm from './components/RegForm';
+import LogForm from './components/LogForm';
 import { Route, Routes } from "react-router-dom";
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
         {/* <Route path="/" element={<RegForm />}/>/ */}
         <Route path="/student/auth/register" element={<RegForm />}/>
+        <Route path="/student/auth/login" element={<LogForm />}/>
     </Routes>
   )
 }
--- client/src/components/LogForm.jsx
+++ client/src/components/LogForm.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 // import Navbar from "./Navbar";
 const RegForm = () => {
-	const [name, setName] = useState("");
 	const [regNo, setRegNo] = useState("");
-	const [blockNo, setBlockNo] = useState("");
 	const [pass, setPass] = useState("");
-	const [roomNo, setRoomNo] = useState("");
 	function postData() {
-		fetch("http://localhost:8000/api/v1/student/auth/register", {
+		fetch("http://localhost:8000/api/v1/student/auth/login", {
 			method: "POST",
 			body: JSON.stringify({
-				name: name,
 				regNo: regNo,
-				block: blockNo,
 				password: pass,
-				roomNo: roomNo,
 			}),
 			headers: {
 				"Content-type": "application/json; charset=UTF-8",
@@ -46,22 +40,6 @@ const RegForm = () => {
 						<div className="mb-4">
 							<label
 								className="block text-white text-sm font-bold mb-2"
-								htmlFor="Name">
-								Name
-							</label>
-							<input
-								className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-								id="Name"
-								type="text"
-								placeholder="Name"
-								onChange={(e) => {
-									setName(e.target.value);
-								}}
-							/>
-						</div>
-						<div className="mb-6">
-							<label
-								className="block text-white text-sm font-bold mb-2"
 								htmlFor="regNo">
 								Register Number
 							</label>
@@ -78,22 +56,6 @@ const RegForm = () => {
 						<div className="mb-6">
 							<label
 								className="block text-white text-sm font-bold mb-2"
-								htmlFor="blockNo">
-								Block name
-							</label>
-							<input
-								className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline"
-								id="blockNo"
-								type="text"
-								placeholder="A"
-								onChange={(e) => {
-									setBlockNo(e.target.value);
-								}}
-							/>
-						</div>
-						<div className="mb-6">
-							<label
-								className="block text-white text-sm font-bold mb-2"
 								htmlFor="pass">
 								Password
 							</label>
@@ -104,22 +66,6 @@ const RegForm = () => {
 								placeholder="Ssssshhhhh"
 								onChange={(e) => {
 									setPass(e.target.value);
-								}}
-							/>
-						</div>
-						<div className="mb-6">
-							<label
-								className="block text-white text-sm font-bold mb-2"
-								htmlFor="roomNo">
-								Room
-							</label>
-							<input
-								className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline"
-								id="roomNo"
-								type="text"
-								placeholder="1101"
-								onChange={(e) => {
-									setRoomNo(e.target.value);
 								}}
 							/>
 						</div>
