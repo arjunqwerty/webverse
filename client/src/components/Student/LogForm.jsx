a6964f7b9caeb1e4bd40fad04c6e325f789d5009
Arjun <arjunqwerty4703@gmail.com>
2023-06-23 22:40:30 +0530

    Student folder

--- client/src/components/Student/LogForm.jsx
+++ client/src/components/Student/LogForm.jsx
@@ -62,7 +62,7 @@ const RegForm = () => {
 							<input
 								className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline"
 								id="pass"
-								type="text"
+								type="password"
 								placeholder="Ssssshhhhh"
 								onChange={(e) => {
 									setPass(e.target.value);
